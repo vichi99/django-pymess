511ed5aae05284bc77ff3c431b4ebf7889ef8415
Jan Silhan <silhan.it@gmail.com>
2023-07-18 19:25:11 -0500

    added is_secret field to SMSTemplate

---
+++ pymess/migrations/0028_migration.py
@@ -0,0 +1,17 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pymess', '0027_migration'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='smstemplate',
+            name='is_secret',
+            field=models.BooleanField(default=False),
+            preserve_default=False,
+        ),
+    ]
